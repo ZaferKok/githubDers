5248cb48989d2e72dc62a0efa6e419382ef69e81
ZaferKok <62161849+ZaferKok@users.noreply.github.com>
2022-12-07 22:26:30 +0100

    Update GithubMain.java

--- githubDers/src/githubGiris/GithubMain.java
+++ githubDers/src/githubGiris/GithubMain.java
@@ -7,6 +7,7 @@ public class GithubMain {
 		System.out.println("Merhaba Github");
 		System.out.println("Sana da Merhaba Eclipse");
 		System.out.println("Nasılsın?");
+		System.out.println("İyiyim");
 		
 	}
 
